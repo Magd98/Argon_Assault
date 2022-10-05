bd304c242cd27a30de90ac2100783a4e4f1596d9
Magd98 <magdmohammed998@gmail.com>
2022-10-05 18:42:01 +0200

    WorkWednesday

---
+++ Argon_Assault/Assets/MusicHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicHandler : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        Invoke("LoadNextScene", 4f);
+    }
+
+    // Update is called once per frame
+   void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
