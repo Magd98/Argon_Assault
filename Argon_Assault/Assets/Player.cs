bb9e1aa60e645f12a7ee7a6938538390fce46850
Magd98 <magdmohammed998@gmail.com>
2022-10-06 18:10:31 +0200

    WorkThursday

---
+++ Argon_Assault/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5.13f;
+    [Tooltip("In m")] [SerializeField] float yRange =2.74f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        print(xOffsetThisFrame);
+        float RawXPos = transform.localPosition.x + xOffsetThisFrame;
+        float clampedXPos = Mathf.Clamp(RawXPos, -xRange, xRange);
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffsetThisFrame = yThrow * xSpeed * Time.deltaTime;
+        print(yOffsetThisFrame);
+        float RawYPos = transform.localPosition.y + yOffsetThisFrame;
+        float clampedYPos = Mathf.Clamp(RawYPos, -yRange, yRange);
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
+}
