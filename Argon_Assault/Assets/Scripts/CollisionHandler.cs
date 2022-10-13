a5044fc8b55f84309e7eda4a30371a394c4aadd3
Magd98 <magdmohammed998@gmail.com>
2022-10-13 14:35:02 +0200

    WorkWednesday

---
+++ Argon_Assault/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print("You triggered Something");
+        StartDeathSequence();
+    }
+
+    void StartDeathSequence()
+    {
+        print("Player Dead");
+        SendMessage("OnPlayerDeath");
+        
+    }
+}
---
+++ Argon_Assault/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{   [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float Speed = 10f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5.13f;
+    [Tooltip("In m")] [SerializeField] float yRange =2.74f;
+    
+    [Header("Screen-position based")]
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -25f;
+
+    [Header("Control-Throw based")]
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -25f;
+
+    float xThrow, yThrow;
+    bool isControlEnabled = true;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Application.targetFrameRate = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+    }
+    void OnPlayerDeath()
+    {
+        print("Player Controls Frozen");
+        isControlEnabled = false;
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDuetoControl = yThrow * controlPitchFactor;
+        float pitchDuetoPosition = transform.localPosition.y * positionPitchFactor;
+        float pitch = pitchDuetoPosition + pitchDuetoControl;
+
+        float yawDuetoPostion = transform.localPosition.x * positionYawFactor;
+        float yaw = yawDuetoPostion;
+
+        float rollDuetoConrol = xThrow * controlRollFactor;
+        float roll = rollDuetoConrol;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * Speed * Time.deltaTime;
+        float RawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(RawXPos, -xRange, xRange);
+
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * Speed * Time.deltaTime;
+        float RawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(RawYPos, -yRange, yRange);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
