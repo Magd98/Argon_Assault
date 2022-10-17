385da28cf9eb431d2e6abb273485a6fc4a18a55c
Magd98 <magdmohammed998@gmail.com>
2022-10-17 16:05:16 +0200

    WorkSunday

---
+++ Argon_Assault/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    void AddNonTriggerBoxCollider()
+    {
+        Collider enemyCollider = gameObject.AddComponent<BoxCollider>();
+        enemyCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void OnParticleCollision(GameObject other)
+    {
+        print("Particle Coliided with" + gameObject.name);
+        Destroy(gameObject);
+    }
+}
